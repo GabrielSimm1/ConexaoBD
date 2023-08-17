9c08afa686d23f721d6c74105e7895ee8ee514d4
GabrielSimm1 <gabriel.simm3@gmail.com>
2023-08-16 21:40:15 -0300

    16-08

--- testando/testando/FormularioListarProduto.cs
+++ testando/testando/FormularioListarProduto.cs
@@ -26,15 +26,16 @@ namespace testando
         {
             int y = 0;
             int x = 0;
+            int quantProd;//guardar a quantidade de produtos
             //tabela de dados
             DataTable dt = new DataTable();
             dt = con.obterDados("select * from produto");//buscando e populando a datatable
             int registros = 0;//ler a quantidade de dados
-            for(registros = 0; registros < dt.Rows.Count; registros++)//varrer os registros da tabela produto
+            for (registros = 0; registros < dt.Rows.Count; registros++)//varrer os registros da tabela produto
             {
                 //criando manualmente
                 Panel produto = new Panel();//criando o painel de produto
-                produto.Location = new Point(x,y);//defino o local
+                produto.Location = new Point(x, y);//defino o local
                 Label idProduto = new Label();//crio uma label
                 idProduto.Name = "id_produto";
                 idProduto.Text = dt.Rows[registros][0].ToString();//mostra o registro
@@ -58,16 +59,34 @@ namespace testando
                 TextBox quant = new TextBox();
                 quant.Name = "qtde";
                 quant.Location = new Point(20, 110);
+                quantProd = Convert.ToInt32(dt.Rows[registros][3].ToString());
+                quant.Leave += new EventHandler((sender1, e1) => quantLeave(sender1, e1, quant.Text, quantProd));
+                if (!string.IsNullOrEmpty(quant.Text))
+                {
+                    if(Convert.ToInt32(quant) <= quantProd)
+                    {
+                        MessageBox.Show("Quantidade indisponível", "Alerta");                    }      
+                }
+                
+                if (quantProd > 0)
+                {
+                    quant.Enabled = true;
+                }
+                else
+                {
+                    quant.Enabled = false;
+                }
+
 
                 //adicionando os componentes ao painel
                 Button registrar = new Button();
                 registrar.Name = "Selecionar";
                 registrar.Text = "Selecionar";
                 registrar.Font = new Font("Arial", 8, FontStyle.Bold);
-                registrar.Location = new Point(20,140);
-                
-                registrar.Click += new EventHandler((sender1, e1) => selecionarClick(sender1, e1, idProduto.Text)); 
-                
+                registrar.Location = new Point(20, 140);
+
+                registrar.Click += new EventHandler((sender1, e1) => selecionarClick(sender1, e1, idProduto.Text));
+
                 produto.Controls.Add(preco);
                 produto.Controls.Add(foto);
                 produto.Controls.Add(idProduto);
@@ -76,12 +95,22 @@ namespace testando
                 produto.Controls.Add(quant);
                 flowLayoutPanel1.Controls.Add(produto);//adiciono cada produto da consulta ao painel
                 y += 100;
-                x = 0; 
+                x = 0;
             }
         }
         private void selecionarClick(object? sender, EventArgs e, string? id)
         {
             MessageBox.Show("Produto selecionado: " + id);
+        }
+        private void quantLeave(object? sender, EventArgs e, string? qtde, int qtdeProd)
+        {
+            if (!string.IsNullOrEmpty(qtde))
+            {
+                if(Convert.ToInt32(qtde) > qtdeProd || Convert.ToInt32(qtde) <= 0)
+                {
+                    MessageBox.Show("Quantidade indisponível", "Alerta");
+                }
+            }
         }
     }
 }
